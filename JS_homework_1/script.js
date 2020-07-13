ca9582c57fb9ad4a827ad2ed3380f3cffbd8d3f8
OlgaFedorovich <63749183+OlgaFedorovich@users.noreply.github.com>
2020-07-13 18:17:14 +0300

    First homework completed

---
+++ JS_homework_1/script.js
@@ -0,0 +1,108 @@
+//Задание_2
+let a1 = 5 / 3,
+    a2 = 3 / 5,
+    a3 = 5 + '3',
+    a4 = '5' - 3,
+    a5 = 75 + 'кг',
+    a6 = 785 * 653,
+    a7 =  100 / 25,
+    a8 =  0 * 0,
+    a9 = 0 / 2,
+    a10 = 89 / 0,
+    a11 = 98 + 2,
+    a12 = 5 - 98,
+    a13 = (8 + 56 * 4) / 5,
+    a14 = (9 - 12) * 7 / (5 + 2),
+    a15 = +"123",
+    a16 = 1 || 0;
+    a17 = false || true;
+    a18 = true > 0;
+
+console.log(a1, typeof a1);
+console.log(a2, typeof a2);
+console.log(a3, typeof a3);
+console.log(a4, typeof a4);
+console.log(a5, typeof a5);
+console.log(a6, typeof a6);
+console.log(a7, typeof a7);
+console.log(a8, typeof a8);
+console.log(a9, typeof a9);
+console.log(a10, typeof a10);
+console.log(a11, typeof a11);
+console.log(a12, typeof a12);
+console.log(a13, typeof a13);
+console.log(a14, typeof a14);
+console.log(a15, typeof a15);
+console.log(a16, typeof a16);
+console.log(a17, typeof a17);
+console.log(a18, typeof a18);
+
+//Задание_3 
+
+let width = 23,
+    height = 10,
+    SPryam = width * height;
+
+console.log(SPryam + 'см');
+
+//Задание_4
+
+let heightCilindra = 10;
+    diametrCilindra = a7;
+const Pi = 3.14;
+let VCilindra = Pi * diametrCilindra * diametrCilindra / 4 * heightCilindra;
+console.log(VCilindra + 'м');
+
+//Задание_5
+
+let r = 5;
+let SKruga = Pi * r * r;
+console.log(SKruga + 'см');
+
+//Задание_6 
+
+let aTrap = 5,
+    bTrap = 7,
+    hTrap = 10,
+    STrap = (aTrap + bTrap) / 2 * hTrap;
+
+console.log(STrap + 'см');
+
+//Задание_7 
+
+let S = 2000000,
+    p = 10 / 100,
+    years = 5,
+    Pereplata = S * p * years;
+
+console.log(Pereplata +'руб');
+
+//Задание_8
+
+/*a+2(x-b)=16;
+b(x+15)=a+6x;
+x+2x+ax+bx=23780.*/
+
+let A = 8,
+    B = 3,
+    X1 = ((16 - A) / 2) + B,
+    X2 = (A - (15 * B)) / (B - 6),
+    X3 = 23780 / (1 + 2 + A + B);
+
+console.log(X1);
+console.log(X2);
+console.log(X3);
+
+
+
+
+
+
+
+    
+
+
+
+
+
+    
